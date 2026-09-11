0b64ca6136a75675149df2d45c081d61e0205daa
gus3000 <gus3000spam@gmail.com>
2026-09-11 22:19:55 +0200

    feat: directional digging, cool sprites

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-const Utils := preload("res://Scripts/utils.gd")
 const Direction := Utils.Direction
 
 @export var SPEED: int = 100
@@ -10,6 +9,7 @@ const Direction := Utils.Direction
 @export var JUMP_FORCE: int = 200
 @export var HOLDING_JUMP_FORCE: int = 400
 
+var facing_direction: Direction = Direction.RIGHT
 var mining: bool = false
 var jumping: bool = false
 var holding_jump: bool = false
@@ -33,6 +33,8 @@ func _process(delta: float) -> void:
 
 func handle_horizontal_movement() -> void:
 	velocity.x = Input.get_axis("left", "right") * SPEED
+	if velocity.x != 0:
+		facing_direction = Direction.LEFT if velocity.x < 0 else Direction.RIGHT
 
 func handle_vertical_movement() -> void:
 	if is_on_floor():
@@ -40,29 +42,35 @@ func handle_vertical_movement() -> void:
 
 	jumping = not jumping and Input.is_action_just_pressed("jump")
 	holding_jump = Input.is_action_pressed("jump")
+	var looking: float = Input.get_axis("up", "down")
+	if looking != 0:
+		facing_direction = Direction.UP if looking < 0 else Direction.DOWN
 
+#region ///mining
 func handle_mining(direction: Direction) -> void:
 	var now: int = Time.get_ticks_msec()
 	if now - last_mined_block_timestamp >= MINING_COOLDOWN:
 		last_mined_block_timestamp = now
 		mine_block(direction)
 
-func get_mined_block(direction: Direction):
-	#TODO
-	pass
-
-func mine_block(direction: Direction) -> void:
+func get_mined_point(direction: Direction) -> Dictionary:
 	var space_state := get_world_2d().direct_space_state
 	var query := PhysicsRayQueryParameters2D.create(global_position,
 			global_position + Utils.vector2_from_direction(direction) * RANGE)
 	query.exclude = [self]
 
 	var result := space_state.intersect_ray(query)
-	print(result)
-	if not "position" in result or not "collider" in result:
+	# print(result)
+	return result
+
+func mine_block(direction: Direction) -> void:
+	print("trying to mine", Utils.string_from_direction(direction))
+	var collide_point := get_mined_point(direction)
+	if not "position" in collide_point or not "collider" in collide_point:
 		return
-	var collided := result.collider as MineableTimeMap
-	collided.mine_block(result.position)
+	var collided := collide_point.collider as MineableTimeMap
+	collided.mine_block(collide_point.position)
+#endregion
 
 func handle_jumping(delta: float) -> void:
 	if jumping:
@@ -75,9 +83,10 @@ func handle_jumping(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	if mining:
-		handle_mining(Direction.DOWN)
+		handle_mining(facing_direction)
 	if jumping or holding_jump:
 		handle_jumping(delta)
 	velocity.y = min(velocity.y + GRAVITY * delta, GRAVITY)
+	# print("facing direction :", Utils.string_from_direction(facing_direction))
 	move_and_slide()
 	pass
--- Scripts/utils.gd
+++ Scripts/utils.gd
@@ -3,7 +3,7 @@ class_name Utils
 enum Direction {UP, DOWN, LEFT, RIGHT}
 const RAY_LENGTH: int = 1
 
-static func vector2_from_direction(direction: Utils.Direction) -> Vector2:
+static func vector2_from_direction(direction: Direction) -> Vector2:
 	match direction:
 		Utils.Direction.UP:
 			return Vector2(0, -RAY_LENGTH)
@@ -14,4 +14,16 @@ static func vector2_from_direction(direction: Utils.Direction) -> Vector2:
 		Utils.Direction.RIGHT:
 			return Vector2(+RAY_LENGTH, 0)
 	return Vector2(0, 0)
-					
+
+static func string_from_direction(direction: Direction)->String:
+	match direction:
+		Direction.UP:
+			return "UP"
+		Direction.DOWN:
+			return "DOWN"
+		Direction.LEFT:
+			return "LEFT"
+		Direction.RIGHT:
+			return "RIGHT"
+		_:
+			return "UNKOWN"

ff87d87860903f0b3a8e8a8adfd6d128afa785a4
gus3000 <gus3000spam@gmail.com>
2026-09-12 13:48:25 +0200

    feat: better camera

--- Scripts/camera_rig.gd
+++ Scripts/camera_rig.gd
@@ -1,18 +1,58 @@
 extends Camera2D
 
-@export var target:Node2D
-@export var max_speed:int = 100
+@export var target: Node2D
+@export var still_shape: Rect2i
 
-# Called when the node enters the scene tree for the first time.
+var target_local_coords: Vector2:
+	get: return to_local(target.global_position)
+
 func _ready() -> void:
-	pass # Replace with function body.
+	pass  # Replace with function body.
 
+func _draw() -> void:
+	if not OS.is_debug_build():
+		return
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	var color: Color = Color.RED
+	var width: float = 1
+	var top_left := still_shape.position
+	var top_right := Vector2i(top_left.x + still_shape.size.x, top_left.y)
+	var bottom_left := Vector2i(top_left.x, top_left.y + still_shape.size.y)
+	var bottom_right := Vector2i(top_left.x + still_shape.size.x, top_left.y + still_shape.size.y)
+
+	draw_circle(top_left, width, color, true, width, false)
+	draw_circle(top_right, width, color, true, width, false)
+	draw_circle(bottom_left, width, color, true, width, false)
+	draw_circle(bottom_right, width, color, true, width, false)
+
+	draw_line(top_left, top_right, color, width, false)
+	draw_line(top_left, bottom_left, color, width, false)
+	draw_line(bottom_right, bottom_left, color, width, false)
+	draw_line(bottom_right, top_right, color, width, false)
+
+	draw_line(Vector2.ZERO, get_travel_vector(0.05), Color.DARK_CYAN, 2, false)
+	pass
+
+func get_travel_vector(delta: float) -> Vector2:
+
+	var local := target_local_coords
+
+	var x: float = 0
+	var y: float = 0
+
+	if local.x < still_shape.position.x:
+		x = local.x - still_shape.position.x
+	elif local.x > still_shape.end.x:
+		x = local.x - still_shape.end.x
+
+	if local.y < still_shape.position.y:
+		y = local.y - still_shape.position.y
+	elif local.y > still_shape.end.y:
+		y = local.y - still_shape.end.y
+
+	return Vector2(x, y)
+
 func _process(delta: float) -> void:
-	var travel_vector: Vector2 = target.position - position
-	var max_speed_ratio: float = travel_vector.length() / (max_speed*delta)
-	if max_speed_ratio > 1:
-		travel_vector /= max_speed_ratio
+	var travel_vector := get_travel_vector(delta)
 	position += travel_vector
 	pass	

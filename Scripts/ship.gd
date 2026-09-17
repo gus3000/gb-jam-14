2d4fc775d04095122f6d26ecf41746fb15f9443e
gus3000 <gus3000spam@gmail.com>
2026-09-17 08:54:20 +0200

    feat: rework sign to use interactible

--- Scripts/ship.gd
+++ Scripts/ship.gd
@@ -1,9 +1,6 @@
-extends Area2D
+extends Node2D
 
 const KeyObjectType := KeyObject.KeyObjectType
-
-@onready var label: Label = $Sign/Label
-@onready var arrow: Sprite2D = $Sign/Arrow
 
 const SHOP_MENU: PackedScene = preload("res://Scenes/shop_menu.tscn")
 
@@ -13,30 +10,6 @@ signal enter_ship
 
 func _ready() -> void:
 	enter_ship.connect(GameController.enter_ship)
-	label.hide()
-	arrow.hide()
-
-func _on_body_entered(body: Node2D) -> void:
-	if body is Player:
-		shop_reached.emit()
-
-
-func _on_body_exited(body: Node2D) -> void:
-	if body is Player:
-		shop_left.emit()
-
-
-func _on_sign_body_entered(body: Node2D) -> void:
-	if body is Player:
-		label.show()
-		arrow.show()
-
-
-func _on_sign_body_exited(body: Node2D) -> void:
-	if body is Player:
-		label.hide()
-		arrow.hide()
-
 
 func enter() -> void:
 	if not GameController.player.has_key_object(KeyObjectType.SHIP_KEY):

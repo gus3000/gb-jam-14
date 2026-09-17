c392f0367c68c55fb20af5cc4c029f21c5df0d77
gus3000 <gus3000spam@gmail.com>
2026-09-17 09:19:26 +0200

    feat: add bedrock under base that gets reapplied after earthquake

--- Scripts/diggable_tile_map.gd
+++ Scripts/diggable_tile_map.gd
@@ -9,16 +9,24 @@ enum TerrainType {
 
 @export var ground_boundaries: Rect2i = Rect2i(0, 8, 50, 50)
 
-var whole_map_pattern: TileMapPattern
+# var whole_map_pattern: TileMapPattern
+var crust_pattern: TileMapPattern
 var terrains: Dictionary[int, TerrainType] = {}
 
 func _ready():
 	GameController.world_shuffle.connect(earthquake)
 
-	whole_map_pattern = TileMapPattern.new()
+	crust_pattern = TileMapPattern.new()
+
+	var crust_line: Array[Vector2i] = []
 	for x in range(ground_boundaries.size.x):
-		for y in range(ground_boundaries.size.y):
-			whole_map_pattern.set_cell(Vector2i(x, y),)
+		crust_line.push_back(Vector2i(ground_boundaries.position.x + x, ground_boundaries.position.y))
+	crust_pattern = get_pattern(crust_line)
+	
+	# whole_map_pattern = TileMapPattern.new()
+	# for x in range(ground_boundaries.size.x):
+	# 	for y in range(ground_boundaries.size.y):
+	# 		whole_map_pattern.set_cell(Vector2i(x, y),)
 
 	# sanity check
 	for terrain in tile_set.get_terrains_count(0):
@@ -80,10 +88,12 @@ func earthquake() -> void:
 	for y in range(ground_boundaries.position.y, ground_boundaries.end.y):
 		for x in range(ground_boundaries.position.x, ground_boundaries.end.x):
 			if y == ground_boundaries.position.y:
-				set_cell(Vector2i(x,y), 1, Vector2i(3,4))
 				continue
 			var chosen_cell = Utils.random_with_weights(stone_variations_coords, stone_variations_probabilities)
 			set_cell(Vector2i(x, y), 1, chosen_cell)
+
+	set_pattern(ground_boundaries.position, crust_pattern)
+	
 	# set_cell(Vector2i(x, y), 1, Vector2i(7, 5))
 	# set_cells_terrain_connect(to_replace_with_stone, 0, 1)
 	var after: int = Time.get_ticks_msec()
--- Scripts/shopkeeper.gd
+++ Scripts/shopkeeper.gd
@@ -1,2 +1,7 @@
 extends Node2D
 
+
+
+func _on_sign_interact() -> void:
+	GameController.ui.queue_string("Hey Captain !")
+	

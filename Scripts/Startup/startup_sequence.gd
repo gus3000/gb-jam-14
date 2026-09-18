688c04d9c9b7242317d02f1d0d5a729d5e994248
hackzorvath <61759498+hackzorvath@users.noreply.github.com>
2026-09-18 15:22:09 -0600

    Add: intro scene and app icon

--- Scripts/Startup/startup_sequence.gd
+++ Scripts/Startup/startup_sequence.gd
@@ -1,9 +1,27 @@
 extends Node2D
 
-@export var play_startup_sequence:bool = true
-var animation_player:AnimationPlayer 
+const STEP_TIME := 0.05
+const STEP_SIZE := 4
+const TARGET_Y := 56
+
+@export var play_startup_sequence := true
+
+@onready var background: ColorRect = $Background
+@onready var nontendo_label: Label = $NontendoLabel
+@onready var ninten_ding: AudioStreamPlayer2D = $NintenDing
+
+
 
 func _ready():
-	animation_player = $Text/AnimationPlayer
-	if play_startup_sequence:
-		animation_player.play("Startup")
+	descend()
+	ninten_ding.play()
+
+func descend() -> void:
+	while nontendo_label.position.y < TARGET_Y:
+		nontendo_label.position.y += STEP_SIZE
+		
+		await get_tree().create_timer(STEP_TIME).timeout
+		
+func _process(delta: float) -> void:
+	if ninten_ding.finished:
+		nontendo_label.visible = false

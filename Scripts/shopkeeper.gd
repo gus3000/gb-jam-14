71596cfee0c68398d9677921872612164983d7c5
gus3000 <gus3000spam@gmail.com>
2026-09-20 11:53:03 +0200

    fix: dialog boxes on large texts

--- Scripts/shopkeeper.gd
+++ Scripts/shopkeeper.gd
@@ -3,5 +3,6 @@ extends Node2D
 
 
 func _on_sign_interact() -> void:
-	GameController.ui.queue_string("Hey Captain !")
+	GameController.ui.queue_string("Hey Captain !", 1)
+	GameController.ui.queue_string("I had big plans\nbut it seems\nthey've\n\"fallen\"\n short")
 	

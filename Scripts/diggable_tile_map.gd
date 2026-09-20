c352fcbde923e19ff890a25c625aff54ae6936b6
gus3000 <gus3000spam@gmail.com>
2026-09-20 15:52:03 +0200

    fix: crash when looking at a non-terrain block

--- Scripts/diggable_tile_map.gd
+++ Scripts/diggable_tile_map.gd
@@ -64,10 +64,14 @@ func get_block_position(collision_position: Vector2) -> Vector2:
 func get_block_at_point(collision_position: Vector2) -> Block:
 	var pos := get_block_map_position(collision_position)
 	var data := get_cell_tile_data(pos)
+	
+	var terrain := data.terrain
+	if terrain < 0:
+		terrain = TerrainType.BEDROCK
 	# print("cell at %s : %s" % [pos, data.terrain])
 
 	# return Block.new(pos.x, pos.y, (pos.y - ground_boundaries.position.y + 1) * toughness_per_depth_unit)
-	return Block.new(pos.x, pos.y, terrains[data.terrain])
+	return Block.new(pos.x, pos.y, terrains[terrain])
 
 # /!\ Resets the ground !
 func earthquake(_intensity: float=-1) -> void:
